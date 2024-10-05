d8d6286fd61a6f75d4b1bcba522445ff7cc0ef05
tannnguci <levantanald@gmail.com>
2024-10-05 16:56:57 +0700

    Set up laravel project

---
+++ config/services.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'resend' => [
+        'key' => env('RESEND_KEY'),
+    ],
+
+    'slack' => [
+        'notifications' => [
+            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
+            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+        ],
+    ],
+
+    'firebase' => [
+        'credentials' => [
+            'type' => env('FIREBASE_TYPE'),
+            'project_id' => env('FIREBASE_PROJECT_ID'),
+            'private_key_id' => env('FIREBASE_PRIVATE_KEY_ID'),
+            'private_key' => env('FIREBASE_PRIVATE_KEY'),
+            'client_email' => env('FIREBASE_CLIENT_EMAIL'),
+            'client_id' => env('FIREBASE_CLIENT_ID'),
+            'auth_uri' => env('FIREBASE_AUTH_URI'),
+            'token_uri' => env('FIREBASE_TOKEN_URI'),
+            'auth_provider_x509_cert_url' => env('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+            'client_x509_cert_url' => env('FIREBASE_CLIENT_X509_CERT_URL'),
+            'universe_domain' => env('FIREBASE_UNIVERSE_DOMAIN'),
+        ],
+    ],
+
+];
